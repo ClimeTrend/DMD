3ef8d33ee7db7872cef99cf1448a0d62bb86767f
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2024-05-24 15:30:54 +0100

    function to convert xarray dataset to numpy array

--- src/WeatherDMD/data_pipeline.py
+++ src/WeatherDMD/data_pipeline.py
@@ -33,3 +33,32 @@ def load_data(file_name: str):
     else:
         raise ValueError("File format not supported")
     return ds
+
+
+def dataset_to_array(ds: xr.Dataset, var_name: str, level: int = None):
+    """
+    Convert xarray dataset to numpy array.
+
+    Parameters
+    ----------
+    ds : xarray.Dataset
+        Dataset to convert.
+    var_name : str
+        Variable name to extract from the dataset.
+    level : int, optional
+        Level to extract from the dataset. If not specified, the first level is extracted.
+
+    Returns
+    -------
+    data : numpy.ndarray
+        Numpy array with the data extracted from the dataset, with dimensions (time, lat, lon).
+    """
+
+    try:
+        if level is None:
+            data = ds[var_name].isel(level=0).values
+        else:
+            data = ds[var_name].sel(level=level).values
+        return data
+    except Exception as e:
+        print(f"Error converting dataset to array: {e}")
