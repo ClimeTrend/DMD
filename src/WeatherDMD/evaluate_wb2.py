1f05a3a492d8b50190ff8aeec9e030a7b6409503
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2024-07-03 15:39:26 +0100

    start writing function to evaluate wb2

--- src/WeatherDMD/evaluate_wb2.py
+++ src/WeatherDMD/evaluate_wb2.py
@@ -1,8 +1,12 @@
 import xarray as xr
+import os
+import numpy as np
+from pyprojroot import here
 from weatherbench2 import config
 from weatherbench2.evaluation import evaluate_in_memory, evaluate_with_beam
 from weatherbench2.metrics import RMSESqrtBeforeTimeAvg, SpatialMSE
 from weatherbench2.regions import SliceRegion
+from WeatherDMD.data_pipeline import load_data
 
 
 def set_up_data_config(
@@ -15,10 +19,14 @@ def set_up_data_config(
     end_date: str,
 ) -> config.Data:
 
+    output_file_prefix = os.path.basename(forecast_path)
+    output_file_prefix, _ = os.path.splitext(output_file_prefix)
+
     paths_config = config.Paths(
         forecast=forecast_path,
         obs=obs_path,
         output_dir=output_dir,
+        output_file_prefix=output_file_prefix,
     )
 
     selection_config = config.Selection(
@@ -64,12 +72,32 @@ def set_up_eval_config(regions: dict = None) -> dict:
 def evaluate_wb2(
     obs_path: str,
     forecast_path: str,
-    output_dir: str,
-    variables: list,
-    levels: list,
-    start_date: str,
-    end_date: str,
+    output_dir: str = None,
+    variables: list = None,
+    levels: list = None,
+    start_date: str = None,
+    end_date: str = None,
     regions: dict = None,
     use_beam: bool = False,
 ) -> None:
-    pass
+
+    forecast = load_data(forecast_path)
+
+    if output_dir is None:
+        output_dir = os.path.join(here(), "data/weatherbench2")
+
+    if variables is None:
+        variables = [i for i in forecast.data_vars]
+        variables = variables[0]
+
+    if levels is None:
+        levels = [i for i in forecast[variables].levels]
+        levels = levels[0]
+
+    if start_date is None:
+        start_date = forecast.time.values[0]
+        start_date = np.datetime_as_string(start_date, unit="D")
+
+    if end_date is None:
+        end_date = forecast.time.values[-1]
+        end_date = np.datetime_as_string(end_date, unit="D")
