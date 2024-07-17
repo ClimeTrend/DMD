a2f07a8124263698ce4d98c52f48eb63f0c2df98
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2024-07-17 10:28:39 +0100

    more work on evaluate wb2 tests

--- tests/evaluate_wb2/test_evaluate_wb2.py
+++ tests/evaluate_wb2/test_evaluate_wb2.py
@@ -1,8 +1,17 @@
 from WeatherDMD.evaluate_wb2 import evaluate_wb2
+from pyprojroot import here
+import os
 
 
 def test_evaluate_wb2():
     """
     Test the evaluate_wb2 function.
     """
-    pass
+    obs_path = os.path.join(
+        here(), "tests/evaluate_wb2/data/input/era5_slice_test.zarr"
+    )
+    forecast_path = os.path.join(
+        here(), "tests/evaluate_wb2/data/input/era5_dmd_forecast_test.zarr"
+    )
+
+    evaluate_wb2(obs_path, forecast_path)
