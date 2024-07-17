9e78d0cf0cafd6c740c401900e8ea941a3721a01
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2024-07-17 15:03:38 +0100

    need to save netcdf to zarr for wb2 to work

--- tests/evaluate_wb2/test_evaluate_wb2.py
+++ tests/evaluate_wb2/test_evaluate_wb2.py
@@ -1,17 +1,41 @@
 from WeatherDMD.evaluate_wb2 import evaluate_wb2
 from pyprojroot import here
 import os
+import pytest
+import xarray as xr
 
 
-def test_evaluate_wb2():
+input_data_path = os.path.join(here(), "tests/evaluate_wb2/data/input")
+
+
+@pytest.fixture(scope="module")
+def temp_data():
+    """
+    Temporarily save NetCDF files as Zarr files for testing.
+    """
+    obs_path = os.path.join(input_data_path, "era5_slice_test.nc")
+    forecast_path = os.path.join(input_data_path, "era5_dmd_forecast_test.nc")
+
+    obs = xr.open_dataset(obs_path)
+    forecast = xr.open_dataset(forecast_path)
+
+    obs_path = os.path.join(input_data_path, "era5_slice_test.zarr")
+    forecast_path = os.path.join(input_data_path, "era5_dmd_forecast_test.zarr")
+
+    obs.to_zarr(obs_path)
+    forecast.to_zarr(forecast_path)
+
+    yield
+
+    os.system(f"rm -r {obs_path}")
+    os.system(f"rm -r {forecast_path}")
+
+
+def test_evaluate_wb2(temp_data):
     """
     Test the evaluate_wb2 function.
     """
-    obs_path = os.path.join(
-        here(), "tests/evaluate_wb2/data/input/era5_slice_test.zarr"
-    )
-    forecast_path = os.path.join(
-        here(), "tests/evaluate_wb2/data/input/era5_dmd_forecast_test.zarr"
-    )
+    obs_path = os.path.join(input_data_path, "era5_slice_test.zarr")
+    forecast_path = os.path.join(input_data_path, "era5_dmd_forecast_test.zarr")
 
     evaluate_wb2(obs_path, forecast_path)
